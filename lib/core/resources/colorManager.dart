2b87c02ff32cfc4243b558e0b81af67f21755c75
omar khaled <omarksas12345@gmail.com>
2025-11-13 17:51:09 +0200

    + responsive design + add libraries

---
+++ lib/core/resources/colorManager.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+ static const Color primaryLightColor = Color(0xff171717);
+ static const Color primaryDarkColor = Color(0xff171717);
+}
--- lib/news_app/news_app.dart
+++ lib/news_app/news_app.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/resources/app_style.dart';
+import 'package:news_app/core/resources/routesManager.dart';
+import 'package:news_app/ui/home/screen/home_screen.dart';
 
 class NewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp() ;
+    return ScreenUtilInit(
+      designSize: const Size(393, 852), // size of the design on figma
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder:(context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {
+            Routesmanager.home : (_)=>HomeScreen(),
+          },
+          initialRoute: Routesmanager.home,
+          theme: AppStyle.lightTheme,
+          darkTheme: AppStyle.darkTheme,
+          themeMode: ThemeMode.light,
+
+        );
+      } ,
+    ) ;
   }
 }
